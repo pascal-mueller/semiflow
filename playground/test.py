077bbabfb1777839a1dacd379498655fd8967a95
Pascal Müller <mueller.pascal@hotmail.com>
2025-04-17 21:50:23 +0200

    added very rudimentary "comp. graph" functionality to node.

--- playground/test.py
+++ playground/test.py
@@ -1,10 +1,15 @@
 from semiflow import Node
 
 x = Node([1, 2, 3])
-y = Node([1, 2, 3])
+y = Node([4, 5, 6])
+z = Node([7, 8, 9])
 
 # Works
-print(x + y)
+r: Node = x + y + z
+
+print(r)
+
+breakpoint()
 
 # Fails
 # print(x + 2)
--- semiflow/node.py
+++ semiflow/node.py
@@ -1,16 +1,30 @@
+from __future__ import annotations
+from typing import Optional, List
+
 import torch
 
 
 class Node:
-    def __init__(self, data):
-        self.tensor = torch.tensor(data)
+    def __init__(
+        self,
+        data,
+        operation: Optional[str] = None,
+        parents: Optional[List[Node]] = None,
+    ) -> None:
+        self.tensor: torch.tensor = torch.tensor(data)
+        self.operation: Optional[str] = operation
+        self.parents: List[Node] = parents or []
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.tensor)
 
-    def __add__(self, other_node):
+    def __add__(self, other_node) -> Node:
         if isinstance(other_node, Node):
-            return Node(self.tensor + other_node.tensor)
+            return Node(
+                self.tensor + other_node.tensor,
+                operation="add",
+                parents=[self, other_node],
+            )
         else:
             raise TypeError(
                 f"Unsupported type for addition with Node. Can't add {type(other_node)} to Node."
