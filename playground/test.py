eeb9439f3908145bf367bf4ca42246d060a1f4d4
Pascal Müller <mueller.pascal@hotmail.com>
2025-04-18 14:51:34 +0200

    added muliplication to node and some more tensor arguments

--- playground/test.py
+++ playground/test.py
@@ -1,15 +1,14 @@
 from semiflow import Node
 
-x = Node([1, 2, 3])
-y = Node([4, 5, 6])
-z = Node([7, 8, 9])
 
-# Works
-r: Node = x + y + z
+x = Node([1])
+y = Node([2])
+z = Node([3])
 
-print(r)
+h1 = x + y  # 1 + 2 = 3
+h2 = 2 * y + 3 * z  # 2*2 + 3*3 = 13
+L = h1 + h2  # 3 + 13 = 16
+
+print(f"Result: {L}")
 
 breakpoint()
-
-# Fails
-# print(x + 2)
--- semiflow/node.py
+++ semiflow/node.py
@@ -7,25 +7,92 @@ import torch
 class Node:
     def __init__(
         self,
-        data,
+        data: torch.Tensor,
         operation: Optional[str] = None,
         parents: Optional[List[Node]] = None,
+        dtype: Optional[torch.dtype] = None,
+        device: Optional[torch.device] = None,
+        requires_grad: Optional[bool] = False,
+        pin_memory: Optional[bool] = False,
     ) -> None:
-        self.tensor: torch.tensor = torch.tensor(data)
+        self.tensor: torch.tensor = torch.as_tensor(data, dtype=dtype, device=device)
         self.operation: Optional[str] = operation
         self.parents: List[Node] = parents or []
+        self.grad: Optional[torch.Tensor] = None
 
     def __repr__(self) -> str:
         return str(self.tensor)
 
     def __add__(self, other_node) -> Node:
         if isinstance(other_node, Node):
+            result: torch.Tensor = self.tensor + other_node.tensor
+
+            if self.parents is not None:
+                pass
             return Node(
-                self.tensor + other_node.tensor,
+                result,
                 operation="add",
                 parents=[self, other_node],
+                dtype=result.dtype,
+                device=result.device,
             )
         else:
             raise TypeError(
                 f"Unsupported type for addition with Node. Can't add {type(other_node)} to Node."
             )
+
+    def __sub__(self, other_node) -> Node:
+        if isinstance(other_node, Node):
+            result: torch.Tensor = self.tensor - other_node.tensor
+
+            return Node(
+                result,
+                operation="sub",
+                parents=[self, other_node],
+                dtype=result.dtype,
+                device=result.device,
+            )
+        else:
+            raise TypeError(
+                f"Unsupported type for addition with Node. Can't subtract {type(other_node)} to Node."
+            )
+
+    def __mul__(self, other_node) -> Node:
+        if isinstance(other_node, (int, torch.Tensor)):
+            result: torch.Tensor = self.tensor * other_node
+
+            return Node(
+                result,
+                operation="mul",
+                parents=[self, other_node],
+                dtype=result.dtype,
+                device=result.device,
+            )
+        else:
+            raise TypeError(
+                f"Unsupported type for addition with Node. Can't multiply {type(other_node)} with Node."
+            )
+
+    def __rmul__(self, other_node) -> Node:
+        if isinstance(other_node, (int, torch.Tensor)):
+            result: torch.Tensor = self.tensor * other_node
+
+            return Node(
+                result,
+                operation="rmul",
+                parents=[self, other_node],
+                dtype=result.dtype,
+                device=result.device,
+            )
+        else:
+            raise TypeError(
+                f"Unsupported type for addition with Node. Can't multiply {type(other_node)} with Node."
+            )
+
+    def backward(self) -> None:
+        """
+        Backpropagation through the graph.
+        """
+
+        # Assume it's already topologically sorted  for now.
+        pass
