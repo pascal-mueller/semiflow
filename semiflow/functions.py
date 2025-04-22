c95979cde99781cd914fedb357b8030260de5116
Pascal Müller <mueller.pascal@hotmail.com>
2025-04-22 13:23:56 +0200

    added type hints to functions.py

--- semiflow/functions.py
+++ semiflow/functions.py
@@ -1,4 +1,11 @@
-def add_backward(grad_output, a, b):
+from __future__ import annotations
+from typing import List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .node import Node  # Avoid circular import issues
+
+
+def add_backward(grad_output: Node, a, b) -> List[Node]:
     # Local derivatives: df/da = 1, df/db = 1
     grad_a = grad_output * 1.0
     grad_b = grad_output * 1.0
@@ -6,7 +13,7 @@ def add_backward(grad_output, a, b):
     return [grad_a, grad_b]
 
 
-def sub_backward(grad_output, a, b):
+def sub_backward(grad_output: Node, a, b) -> List[Node]:
     # Local derivatives: df/da = 1, df/db = -1
     grad_a = grad_output * 1.0
     grad_b = grad_output * -1.0
@@ -14,7 +21,7 @@ def sub_backward(grad_output, a, b):
     return [grad_a, grad_b]
 
 
-def mul_backward(grad_output, a, b):
+def mul_backward(grad_output: Node, a, b) -> List[Node]:
     # Local derivatives: df/da = b, df/db = a
     grad_a = grad_output * b
     grad_b = grad_output * a
